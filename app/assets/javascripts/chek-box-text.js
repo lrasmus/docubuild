50033e63bb99881df0345e83c48f2fd846ca83a4
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-07-31 22:04:57 -0500

    Got home page matching original design

---
+++ app/assets/javascripts/chek-box-text.js
@@ -0,0 +1,58 @@
+
+$(function () {
+$('#cbxShowHide').click(function(){
+    this.checked?$('#block').show(500):$('#block').hide(500);
+});
+
+$('#cbxShowHide01').click(function(){
+    this.checked?$('#block01').show(500):$('#block01').hide(500);
+});
+
+$('#cbxShowHide02').click(function(){
+    this.checked?$('#block02').show(500):$('#block02').hide(500);
+});
+
+$('#cbxShowHide03').click(function(){
+    this.checked?$('#block03').show(500):$('#block03').hide(500);
+});
+
+$('#cbxShowHide04').click(function(){
+    this.checked?$('#block04').show(500):$('#block04').hide(500);
+});
+
+$('#cbxShowHide05').click(function(){
+    this.checked?$('#block05').show(500):$('#block05').hide(500);
+});
+
+$('#cbxShowHide06').click(function(){
+    this.checked?$('#block06').show(500):$('#block06').hide(500);
+});
+
+$('#cbxShowHide07').click(function(){
+    this.checked?$('#block07').show(500):$('#block07').hide(500);
+});
+
+$('#cbxShowHide08').click(function(){
+    this.checked?$('#block08').show(500):$('#block08').hide(500);
+});
+
+$('#cbxShowHide09').click(function(){
+    this.checked?$('#block09').show(500):$('#block09').hide(500);
+});
+$('#cbxShowHide10').click(function(){
+    this.checked?$('#block10').show(500):$('#block10').hide(500);
+});
+
+
+$('#cbxShowHide11').click(function(){
+    this.checked?$('#block11').show(500):$('#block11').hide(500);
+});
+$('#cbxShowHide12').click(function(){
+    this.checked?$('#block12').show(500):$('#block12').hide(500);
+});
+
+$('#cbxShowHide13').click(function(){
+    this.checked?$('#block09').show(500):$('#block13').hide(500);
+});
+
+});
